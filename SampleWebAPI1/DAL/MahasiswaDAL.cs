a53bc3e437cef7858df669ee89a2c5bea5741d7f
erick <erick@DESKTOP-7DP9Q9N>
2016-10-10 13:35:05 +0700

    menambahkan method untuk mengambil semua data pada table Mahasiswa

--- SampleWebAPI1/DAL/MahasiswaDAL.cs
+++ SampleWebAPI1/DAL/MahasiswaDAL.cs
@@ -2,10 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
+using SampleWebAPI1.Models;
+using System.Data.SqlClient;
 
 namespace SampleWebAPI1.DAL
 {
-    public class MahasiswaDAL
+    public class MahasiswaDAL 
     {
+        private string GetConnectionString()
+        {
+            return 
+                ConfigurationManager
+                .ConnectionStrings["SampleKSIDbConnectionString"]
+                .ConnectionString;
+        }
+
+        public IEnumerable<Mahasiswa> GetAll()
+        {
+            List<Mahasiswa> lstMhs = new List<Mahasiswa>();
+            using (SqlConnection conn = new SqlConnection(GetConnectionString()))
+            {
+                string strSql = @"select * from Mahasiswa order by Nim";
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if(dr.HasRows)
+                {
+                    while(dr.Read())
+                    {
+                        lstMhs.Add(new Mahasiswa
+                        {
+                            Nim = dr["Nim"].ToString(),
+                            Nama = dr["Nama"].ToString(),
+                            Email = dr["Email"].ToString(),
+                            IPK = Convert.ToDouble(dr["IPK"])
+                        });
+                    }
+                }
+
+                dr.Close();
+                cmd.Dispose();
+                conn.Close();
+            }
+            return lstMhs;
+        }
     }
 }
